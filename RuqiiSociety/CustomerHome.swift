519f253a96c515ad0fc5104db25a1b0f68a5a368
lobnasyr <lobna.alzeer@gmail.cpm>
2017-08-21 10:46:55 +0300

    same

--- RuqiiSociety/CustomerHome.swift
+++ RuqiiSociety/CustomerHome.swift
@@ -3,39 +3,35 @@
 //  RuqiiSociety
 //  Created by Maha Alkatheiry on 16/08/2017.
 //  Copyright © 2017 Maha Alkathiery. All rights reserved.
-//
 
 import UIKit
 
 class CustomerHome: UIViewController {
 
     var ViewAppearsAfterLogin = false
-    var CustomerId = ""
-    var CustomerEmail = ""
-    var CustomerName = ""
-    //var orders
-    var CustomerPhone = ""
-    var CustomerPrivatePhone = true
-    
-    
+ 
+    @IBOutlet weak var customerNameLabel: UILabel!
     override func viewWillAppear(_ animated: Bool) {
         if (ViewAppearsAfterLogin == true){
-            Utilities().ShowAlert(title: "تم تسجيل الدخول", msg:" في مجتمع رقي "+CustomerName+" أهلا بك مجددا ", vc: self)
+        
+                let firstName = CustomerInformation.CustomerName.components(separatedBy:" ").first
+                Utilities().ShowAlert(title:"تم تسجيل الدخول",msg:" أهلا بك مجددا "+firstName!+" في مجتمع رقي  ", vc: self)
+         customerNameLabel.text = CustomerInformation.CustomerName
         }
         
-    }
+    }// end of ViewWillAppear function
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
+    }// end of viewDidLoad function
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }// end of idReceiveMemoryWarning function
     
 
 
-}
+}// end of CustomerHome class
--- RuqiiSociety/ExpertHome.swift
+++ RuqiiSociety/ExpertHome.swift
@@ -2,7 +2,7 @@
 //  ExpertHome.swift
 //  RuqiiSociety
 //  Created by Maha Alkatheiry on 13/08/2017.
-//  Copyright © 2017 Maha Alkatheiry. All rights reserved.
+//  Copyright © 2017 Maha Alkatheiry . All rights reserved.
 
 import UIKit
 import Firebase
@@ -11,39 +11,30 @@ import Foundation
 class ExpertHome: UIViewController {
     
 var ViewAppearsAfterLogin = false
- var ExpertId = ""
- var ExpertbankName = ""
- //var ExpertServices = ""
- //var ExpertCompletedOrders = -1
- var ExpertEmail = ""
- var ExperIban = ""
- var ExpertPrivatePhone = true
- var ExpertName = ""
- var ExpertNumOfRating = 0
- //var ExpertPendingOrders = 0
- var ExpertPhone = ""
- //var ExpertPortFolio
- //var ExpertRating = 0
- //var ExpertRatingPoint: Int
- var ExpertTitle = ""
-    
+
     @IBOutlet weak var pendingOrdersCountLabel: UILabel!
 
+   
+    @IBOutlet weak var settingsButton: UIButton!
     @IBOutlet weak var completedOrdersCountLabel: UILabel!
     @IBOutlet weak var ratingCountsLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var fullNameLabel: UILabel!
-    @IBOutlet weak var settingsIcon: UIImageView!
-    @IBOutlet weak var newOrdersIcon: UIImageView!
+  
+    
 
    override func viewWillAppear(_ animated: Bool) {
         if (ViewAppearsAfterLogin == true){
-            Utilities().ShowAlert(title: "تم تسجيل الدخول", msg:" في مجتمع رقي "+ExpertName+" أهلا بك مجددا ", vc: self)
-        }
-        
+            let firstName = ExpertInformation.ExpertName.components(separatedBy:" ").first
+            Utilities().ShowAlert(title:"تم تسجيل الدخول",msg:" أهلا بك مجددا "+firstName!+" في مجتمع رقي  ", vc: self)
+                  }
+   
     }
     
     
+    @IBAction func settingsClicked(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "settings", sender: self)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,33 +44,8 @@ var ViewAppearsAfterLogin = false
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated
     }
     
 
-/* Servicesaddclose
- bankName:
- "مصرف الراجحي"
- completed_orders
- email:
- "3boorah.2014@gmail.com"
- iban:
- "SA6543212345678987654345"
- isPhonePrivate:
- false
- name:
- "عبير الشهري"
- numOfRating:
- 1
- pending_orders
- phone:
- "0569875369"
- portfolio
- rating:
- 3.5
- ratingPoints:
- 3.5
- title:
- " مبرمجة"
-*/
 }//  end of the class
