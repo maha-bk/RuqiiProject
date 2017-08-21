dadabd59bb7833555bf85d6ed0635cb00732fc23
lobnasyr <lobna.alzeer@gmail.cpm>
2017-08-21 10:49:11 +0300

    same

--- RuqiiSociety/CustomerHome.swift
+++ RuqiiSociety/CustomerHome.swift
@@ -30,6 +30,8 @@ class CustomerHome: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }// end of idReceiveMemoryWarning function
     
 
